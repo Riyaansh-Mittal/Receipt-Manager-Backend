ec9bb25007f5c1c55482078b73d4c9af5272f6c2
Riyaansh Mittal <riyaashmittal14@gmail.com>
2025-11-26 15:07:03 +0530

    add static root to settings.py

--- receiptmanager/settings.py
+++ receiptmanager/settings.py
@@ -522,6 +522,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Directory where collectstatic will put all static files (for Render to serve)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
